a0962ef4c48fbf611b26a6a1bf130ef6644ff73e
Stuart Smith <stuartsayswhat@googlemail.com>
2020-12-20 18:09:44 +0000

    new

--- app.py
+++ app.py
@@ -7,16 +7,17 @@ load_dotenv()                   # Load gathers the value from dotenv file.
                                 # variable and they can be conveniently accessed
                                 # via os.getenv()
                                 # Use from_object when configs contain classes
-print("OS Environ before", os.environ["APP_SETTINGS"])
-print(os.getenv("APP_SETTINGS"))
+#os.environ["APP_SETTINGS"]
+#print("OS Environ before", os.environ["APP_SETTINGS"])
+#os.getenv("APP_SETTINGS")
 #print("get", os.getenv("APP_SETTINGS"))
 #config.DevelopmentConfig
 
 app = Flask(__name__)
-app.config.from_object(os.environ["APP_SETTINGS"])
+app.config.from_object(os.environ['APP_SETTINGS'])
 
-#app.config.from_object(os.environ["APP_SETTINGS"])
-#print("OS environ after", os.environ['APP_SETTINGS'])
+app.config.from_object(os.environ["APP_SETTINGS"])
+print("OS environ after", os.environ['APP_SETTINGS'])
 
 @app.route('/')
 def hello():
@@ -28,3 +29,4 @@ def hello_name(name):
 
 if __name__ == '__main__':
     app.run(debug = True)
+    print(os.environ['APP_SETTINGS'])
