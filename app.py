05b32c8559fcd2ecc9e6c3684fd4c3cdc1ec829c
Stuart Smith <stuartsayswhat@googlemail.com>
2021-01-02 12:52:57 +0000

    redis not working

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import operator
 import re
 import nltk
 # import requests library to send external HTTP GET requqests to grab the URL
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
 # Flask request object to handle GET and POST requests within the Flask app.
 from flask_sqlalchemy import SQLAlchemy
 from dotenv import load_dotenv
@@ -35,52 +35,86 @@ q = Queue(connection=conn)
 
 from models import *
 
-#app.config.from_object(os.environ["APP_SETTINGS"])ç
+#app.config.from_object(os.environ["APP_SETTINGS"])
 print("OS environ after", os.environ['APP_SETTINGS'])
 
-@app.route('/', methods = ['GET', 'POST'])
-def index():
+def count_and_save_words(url):
+
     errors = []
+
+    try:
+        r = requests.get(url)
+    except:
+        errors.append(
+            "Unable to get URL. Please make sure it's valid and try again."
+        )
+        return {"error": errors}
+
+    # text processing
+    raw = BeautifulSoup(r.text).get_text()
+    nltk.data.path.append('./nltk_data/')  # set the path
+    tokens = nltk.word_tokenize(raw)
+    text = nltk.Text(tokens)
+
+    # remove punctuation, count raw words
+    nonPunct = re.compile('.*[A-Za-z].*')
+    raw_words = [w for w in text if nonPunct.match(w)]
+    raw_word_count = Counter(raw_words)
+
+    # stop words
+    no_stop_words = [w for w in raw_words if w.lower() not in stops]
+    no_stop_words_count = Counter(no_stop_words)
+
+    # save the results
+    try:
+        result = Result(
+            url=url,
+            result_all=raw_word_count,
+            result_no_stop_words=no_stop_words_count
+        )
+        db.session.add(result)
+        db.session.commit()
+        return result.id
+    except:
+        errors.append("Unable to add item to database.")
+        return {"error": errors}
+
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
     results = {}
     if request.method == "POST":
-        # get URL user has entered
-        try:
-            url = request.form['url']
-            r = requests.get(url)
-            print(r.text)
-        except:
-            errors.append(
-                "Unable to get URL. Please make sure it's valid and try again"
-            )
-        if r:
-            # text processing
-            raw = BeautifulSoup(r.text, 'html.parser').get_text()
-            nltk.data.path.append('./nltk_data/') # set the path
-            tokens = nltk.word_tokenize(raw)
-            text = nltk.Text(tokens)
-            # remove punctuation, count raw words
-            nonPunct = re.compile('.*[A-Za-z].*')
-            raw_words = [w for w in text if nonPunct.match(w)]
-            raw_word_count = Counter(raw_words)
-            # stop words
-            no_stop_words = [w for w in raw_words if w.lower() not in stops]
-            no_stop_words_count = Counter(no_stop_words)
-            results = sorted(
-                no_stop_words_count.items(),
-                key=operator.itemgetter(1),
-                reverse=True
-            )[:10]
-            try:
-                result = Result(
-                    url = url,
-                    result_all = raw_word_count,
-                    result_no_stop_words=no_stop_words_count
-                )
-                db.session.add(result)
-                db.sessions.commit()
-            except:
-                errors.append("Unable to add item to database.")
-    return render_template('index.html', errors = errors, results=results)
+        # this import solves a rq bug which currently exists
+        from app import count_and_save_words
+
+        # get url that the person has entered
+        url = request.form['url']
+        if not url[:8].startswith(('https://', 'http://')):
+            url = 'http://' + url
+        job = q.enqueue_call(
+            func=count_and_save_words, args=(url,), result_ttl=5000
+        )
+        print(job.get_id())
+
+    return render_template('index.html', results=results)
+
+# This id is needed to see if the job is done processing
+# This is the route to handle that!
+@app.route("/results/<job_key>", methods=['GET'])
+def get_results(job_key):
+
+    job = Job.fetch(job_key, connection=conn)
+
+    if job.is_finished:
+        result = Result.query.filter_by(id=job.result).first()
+        results = sorted(
+            result.result_no_stop_words.items(),
+            key=operator.itemgetter(1),
+            reverse=True
+        )[:10]
+        return jsonify(results)
+    else:
+        return "Nay!", 202
 
 @app.route('/<name>')
 def hello_name(name):
--- worker.py
+++ worker.py
@@ -8,10 +8,11 @@ from rq import Worker, Queue, Connection
 load_dotenv() 
 
 listen = ['default']
+#redis_url = os.environ['REDISTOGO_URL']
+#redis_url = os.getenv((os.environ('REDISTOGO_URL')), 'redis://localhost:6379')
 
-redis_url = os.getenv('REDISTOGO_URL', 'redis://localhost:6379')
-
-conn = redis.from_url(redis_url)
+#conn = redis.from_url(redis_url)
+conn = redis.from_url(os.getenv('REDISTOGO_URL'))
 
 if __name__ == '__main__':
     with Connection(conn):
