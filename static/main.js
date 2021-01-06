e0af6ecbc85bb7428adbb8199b7e6eea2bdc2b51
Stuart Smith <stuartsayswhat@googlemail.com>
2021-01-06 16:42:21 +0000

    added spinner

--- static/main.js
+++ static/main.js
@@ -7,48 +7,63 @@
     function($scope, $log, $http, $timeout) {
 
     $scope.getResults = function() {
-
+    $scope.urlerror = false;
+    $scope.submitButtonText = 'Submit';
+    $scope.loading = false;
     $log.log("test");
 
     // get the URL from the input
     var userInput = $scope.url;
 
     // fire the API request
-    $http.post('/start', {"url": userInput}).
+    $http.post('/start', {'url': userInput}).
     success(function(results) {
       $log.log(results);
       getWordCount(results);
-  
+      $scope.wordcounts = null;
+      $scope.loading = true;
+      $scope.submitButtonText = 'Loading...';
+      $scope.urlerror = false;
     }).
     error(function(error) {
       $log.log(error);
     });
 
   };
+  function getWordCount(jobID) {
 
-    function getWordCount(jobID) {
-    var timeout = "";
-  
+    var timeout = '';
+
     var poller = function() {
       // fire another request
       $http.get('/results/'+jobID).
         success(function(data, status, headers, config) {
-            if(status === 202) {
+          if(status === 202) {
             $log.log(data, status);
-            } else if (status === 200){
+          } else if (status === 200){
             $log.log(data);
+            $scope.loading = false;
+            $scope.submitButtonText = "Submit";
             $scope.wordcounts = data;
             $timeout.cancel(timeout);
             return false;
-            }
-            // continue to call the poller() function every 2 seconds
-            // until the timeout is cancelled
-            timeout = $timeout(poller, 2000);
+          }
+          // continue to call the poller() function every 2 seconds
+          // until the timeout is cancelled
+          timeout = $timeout(poller, 2000);
+        }).
+        error(function(error) {
+          $log.log(error);
+          $scope.loading = false;
+          $scope.submitButtonText = "Submit";
+          $scope.urlerror = true;
         });
     };
+
+    poller();
     poller();
   }
-}
+    }
 
 ]);
   
